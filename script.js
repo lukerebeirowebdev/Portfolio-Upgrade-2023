9314ede837800f8190f8d852d56d5960b9787cf3
Luke Rebiro <68423198+lukerebeirowebdev@users.noreply.github.com>
2023-06-24 22:00:34 +0100

    Deleted images and updated CSS

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // toggle icon navigation bar
 
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
 
-menuIcon.onclick = () => {
+menuIcon.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
@@ -12,22 +14,34 @@ menuIcon.onclick = () => {
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
-        let id = getAttribute('id');
+function updateActiveSection() {
+    const sections = Array.from(document.querySelectorAll('section'));
+    const navLinks = Array.from(document.querySelectorAll('header nav a'));
 
-        if (top >= offset && top < offset + height) {
-            // active navbar links
+    // Cache DOM elements and calculations outside the loop
+    const windowTop = window.scrollY;
+
+    sections.forEach(sec => {
+        const offset = sec.offsetTop - 100;
+        const height = sec.offsetHeight;
+        const id = sec.getAttribute('id');
+
+        if (windowTop >= offset && windowTop < offset + height) {
             navLinks.forEach(links => {
-                links
-            })
+                links.classList.remove('active');
+                document.querySelector(`header nav a[href*=${id}]`).classList.add('active');
+            });
         }
     });
 }
 
-let header = document.querySelector('header');
+window.addEventListener('scroll', updateActiveSection);
 
-header.classList.toggle('sticky', window.scrollY > 100);
+
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', toggleStickyClass);
+
+function toggleStickyClass() {
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
